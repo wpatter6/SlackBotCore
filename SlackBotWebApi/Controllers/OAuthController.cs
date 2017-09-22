7c3f8f25cd8288f938aa1745d85ebdbdd60d89ad
wpatter6 <wpatter6@gmail.com>
2017-09-22 11:42:57 -0600

    Adding web api project for getting auth codes

---
+++ SlackBotWebApi/Controllers/OAuthController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SlackBotWebApi.Controllers
+{
+    public class OAuthController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public JsonResult Connect(string code)
+        {
+
+
+            return new JsonResult("ok");
+        }
+    }
+}
